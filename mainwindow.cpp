a3e5536de4d2132448fe244351801b0518ccb5b3
matheuspx <matheustectj@gmail.com>
2024-10-27 18:42:38 -0300

    feat: adicionando sessao sobre

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,7 +4,10 @@
 #include <QFile>
 #include <QFileDialog>
 #include <QTextStream>
-
+#include <QFontDialog>
+#include <QFont>
+#include <QColorDialog>
+#include <QColor>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -31,6 +34,9 @@ void MainWindow::on_actionAbrir_triggered()
     QString nome_arquivo = QFileDialog::getOpenFileName(this, "Abrir", QDir::homePath(), filtro);
     QFile arquivo(nome_arquivo);
     local_arquivo = nome_arquivo;
+    if (nome_arquivo.isEmpty()) {
+        return;
+    }
     if (!arquivo.open(QFile::ReadOnly | QFile::Text)) {
         QMessageBox::warning(this, "", "Tipo de arquivo não suportado");
         return;
@@ -65,3 +71,49 @@ void MainWindow::on_actionSalvar_triggered()
     arquivo.flush();
     arquivo.close();
 }
+
+void MainWindow::on_actionfechar_triggered()
+{
+    close();
+}
+
+void MainWindow::on_actionRecortar_triggered()
+{
+    ui->textEdit->cut();
+}
+
+
+void MainWindow::on_actionColar_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actiondesfazer_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void MainWindow::on_actionrefazer_triggered()
+{
+    ui->textEdit->redo();
+}
+
+
+void MainWindow::on_actionFonte_triggered()
+{
+    bool fonte_ok;
+        QFont fonte=QFontDialog::getFont(&fonte_ok,this);
+    if(fonte_ok){
+            ui->textEdit->setFont(fonte);
+    }else
+        return;
+    }
+
+void MainWindow::on_actionConhe_a_o_autor_triggered()
+{
+    QUrl url("https://github.com/matheuspx/notepad");
+
+    QDesktopServices::openUrl(url);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDesktopServices>
+#include <QUrl>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -24,6 +26,23 @@ private slots:
     void on_actionAbrir_triggered();
 
     void on_actionSalvar_triggered();
+
+    //void on_actionConhe_a_o_projeto_triggered();
+
+    void on_actionfechar_triggered();
+
+    void on_actionRecortar_triggered();
+
+    void on_actionColar_triggered();
+
+    void on_actiondesfazer_triggered();
+
+    void on_actionrefazer_triggered();
+
+    void on_actionFonte_triggered();
+
+
+    void on_actionConhe_a_o_autor_triggered();
 
 private:
     Ui::MainWindow *ui;
